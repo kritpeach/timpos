a9e0bfbc5441a66af29350874ccd0fe097a52665
Krit Chernkwansri <5831305003@lamduan.mfu.ac.th>
2018-09-07 14:39:52 +0700

    Update Firestore date

--- src/service/statistic.js
+++ src/service/statistic.js
@@ -1,3 +1,4 @@
+import firebase from "firebase";
 import firebaseApp from "../connector/firebase";
 
 const generateSalesByCategory = (doneOrderLineItemList) => {
@@ -24,8 +25,8 @@ const getDashboardData = async (
   categoryId,
   restaurantId
 ) => {
-  console.log('startDate', startDate);
-  console.log('endDate', endDate);
+  const startDateTimestamp = new firebase.firestore.Timestamp(startDate.getTime() / 1000, 0);
+  const endDateTimestamp = new firebase.firestore.Timestamp(endDate.getTime() / 1000, 0);
   const restaurantRef = firebaseApp
     .firestore()
     .collection("restaurant")
@@ -34,23 +35,28 @@ const getDashboardData = async (
     .firestore()
     .collection("order")
     .where("restaurant", "==", restaurantRef)
-    .where("createAt", ">=", startDate)
-    .where("createAt", "<=", endDate)
+    .where("createAt", ">=", startDateTimestamp)
+    .where("createAt", "<=", endDateTimestamp)
     .orderBy("createAt")
     .get();
+    /*
+    .where("createAt", ">=", startDate)
+    .where("createAt", "<=", endDate)
+    */
   const orderList = orderListSnap.docs.map(doc => doc.data());
+  console.log("orderList", orderList);
   const orderLineItemListFn = () => {
     const allOrderLineItemList = [].concat(...orderList
-      .filter(order => order.doneAt instanceof Date)
+      .filter(order => order.doneAt instanceof Object)
       .map(order => order.order));
     if (categoryId) {
-      // console.log('categoryId', categoryId);
       return allOrderLineItemList
         .filter(orderLineItem => typeof orderLineItem.menu.categories[categoryId] !== 'undefined');
     }
     return allOrderLineItemList;
   };
   const orderLineItemList = orderLineItemListFn();
+  console.log("orderLineItemList", orderLineItemList);
   const billRefList = orderLineItemList.map(orderLineItem => orderLineItem.billId);
   const billCount = new Set(billRefList).size;
   const doneOrderLineItemList = orderLineItemList.filter(orderLineItem => orderLineItem.done);
